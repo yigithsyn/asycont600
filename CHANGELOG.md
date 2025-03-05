64324d1601aa74ee4f18f5bc19015cd79fda43fe
Hüseyin <yigit.hsyn@gmail.com>
2025-03-05 14:46:45 +0300

    new release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.5.0] - 2025-03-04
+
+### Added
+- Stop command
+
 ## [0.4.0] - 2025-03-04
 
 ### Added
--- src/asycont600/__main__.py
+++ src/asycont600/__main__.py
@@ -20,6 +20,17 @@ def app():
     cont = Asycont600_2()
     cont.connect()
     cont.move(args.axis[0], args.pos[0], "absolute" if args.absolute else "relative", "slow")
+  elif len(sys.argv)>1 and sys.argv[1] == "stop":
+    parser = argparse.ArgumentParser(prog="move", description="Move Axis")
+    parser.add_argument("axis", help="axis name [x, y, z, autslide, azimuth, pol]", type=str, nargs=1)
+    try:
+      args = parser.parse_args(sys.argv[2:])
+    except:
+      print(parser.format_help())
+      exit()
+    cont = Asycont600_2()
+    cont.connect()
+    cont.stop(args.axis[0])
   # elif len(sys.argv)>1 and sys.argv[1] == "server":
   #   parser = argparse.ArgumentParser(prog="server", description="Start HTTP Server")
   #   parser.add_argument("--port", help="tcp port number", default="7001", type=float, nargs=1)
@@ -56,9 +67,9 @@ def app():
   #   app.run()
   else:
     parser = argparse.ArgumentParser(prog="asycont600", description="ASYCONT600 Command line Utility")
-    parser.add_argument("move",   help="move axis", type=str, nargs="?")
-    parser.add_argument("server", help="start http server", type=str, nargs="?")
-    print(parser.format_help().replace("positional arguments","module/function").replace("[move] ","").replace("[server] ","").replace("[swr2gamma] ","").replace("[wlen2freq] ","").replace("[measurement] ","").replace("[propagation] ","").replace("[propagation]","[module/function]"))
+    parser.add_argument("move", help="move axis", type=str, nargs="?")
+    parser.add_argument("stop", help="stop axis", type=str, nargs="?")
+    print(parser.format_help().replace("positional arguments","module/function").replace("[move] ","").replace("[stop] ","").replace("[swr2gamma] ","").replace("[wlen2freq] ","").replace("[measurement] ","").replace("[propagation] ","").replace("[propagation]","[module/function]"))
 
 if __name__ == "__main__":
   app()
